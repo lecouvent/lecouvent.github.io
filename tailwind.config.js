1fd8cfd162e088c74eacb1f6a15b148ba0ddcb3f
François <25376055+fravan@users.noreply.github.com>
2025-04-23 10:28:34 +0200

    New design

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 module.exports = {
   content: ["./index.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        dark: "#9746b3",
+        light: "#d0a9dd",
+        btn: "#583564",
+      },
+    },
   },
   plugins: [],
 };
